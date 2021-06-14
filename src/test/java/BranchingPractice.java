7b624140c3519b2debcf8947efd331091be67e8c
ZhannaSar <zsarsen000@citymail.cuny.edu>
2021-06-14 04:06:12 -1000

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+        //created a new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and iml file
+        //added initial commit by keeping track all files
+        //shared project on github
     }
 
 }
